b2324c9e848e1b92f4981432c26a1a6dd62e962d
bluninjadragon <jwhbootcamp@gmail.com>
2021-04-29 21:50:20 -0500

    update

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,32 @@
 $(document).ready(function () {
     // all your code goes here
 
-   //color rows based on time of day 
-   function updateRowColor() {
+    // user click save button
+    $('.saveBtn').on('click', saveTask);
+   
+    function updateRowColor() {
        // set variable to current hour
        // loop through each row of the class (time-block)
-        // find row time using data attribute (9am ~ 9pm)
+        // find row time using data attribute (9<21 hours)
         // add the past class to $(this).addClass('past')
+   }
+    function saveTask() {
+        alert('saved');
+        // get stuff user put in textarea
+        //save that to localstorage
+        //show message that this was "save to local storage"
+        //hide message after certain amount of time
+    }
 
+   //color rows based on time of day 
+   // use interval to update row color
+    updateRowColor();
 
-   };
+   // load data from local storage and put it in the correct row
+        // #hour-9 and textarea.textContent to value of localstorage.keynumber'hour 9;
+        // #hour-9 and textarea.textContent to value of localstorage.keynumber'hour 9;
+   // display current date on the page - moment (get element by ID)
 
+});
 
-    // display current date on the page - moment (get element by ID)
-
-})
+// event listener
